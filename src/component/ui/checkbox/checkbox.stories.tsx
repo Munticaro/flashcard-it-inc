8b97b0fdf5b94ab9487eb3ddfd40f4cc5b8e0e6c
Munticaro <diskor1996@gmail.com>
2024-03-04 21:58:33 +0300

    delete checkbox

--- src/component/ui/checkbox/checkbox.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { Checkbox } from '@/component/ui/checkbox/checkbox'
-import { Meta } from '@storybook/react'
-
-export default {
-  component: Checkbox,
-  title: 'Components/Checkbox',
-} as Meta<typeof Checkbox>
